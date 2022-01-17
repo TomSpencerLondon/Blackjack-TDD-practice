734a7efafc5994ac2bc5f35af556b9a1dd22332f
Tom <thomas.spencer@codurance.com>
2022-01-17 19:52:34 +0000

    add starter code

---
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -0,0 +1,21 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CardDisplayTest {
+
+    @Test
+    void displayTenAsString() {
+        final Card card = new Card(Suit.CLUBS, Rank.TEN);
+        final String display = Card.display(card.rank(), card.suit());
+        assertThat(display)
+                .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
+    }
+
+    @Test
+    void displayNonTenAsString() {
+
+    }
+}
